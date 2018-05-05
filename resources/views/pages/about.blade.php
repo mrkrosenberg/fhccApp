93711b45dc883287b05de0bfcf4a4c89dbab6caa
Mark Rosenberg <mrk.rosenberg@gmail.com>
2018-05-05 17:05:12 -0600

    adds basic text

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -9,9 +9,11 @@
 
     </head>
     <body>
-        <h1>About</h1>
+        <h1>About Us</h1>
         <p>
-            About Us
+            A local climbing organization based in Jefferson County, Colorado, along the beautiful front range and just 
+            west of Denver. We are here to advocate for climbing, climbing areas and the people who use them.
+            We serve as a mediator between land managers, local governments and the climbers.
         </p>
     </body>
 </html>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -11,9 +11,7 @@
     <body>
         <h1>Welcome to the Foothills Climbing Community</h1>
         <p>
-            A local climbing organization based in Jefferson County, Colorado, along the beautiful front range and just 
-            west of Denver. We are here to advocate for climbing, climbing areas and the people who use them.
-            We serve as a mediator between land managers, local governments and the climbers.
+    
         </p>
     </body>
 </html>
