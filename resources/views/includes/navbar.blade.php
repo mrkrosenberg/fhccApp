8ee4dc595150582daf9c4b5c76b7d4db42a3ab30
Mark Rosenberg <mrk.rosenberg@gmail.com>
2018-05-09 16:21:20 -0600

    adds nav bar, adds calendar to events page

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -1,28 +1,32 @@
     <nav class="navbar navbar-expand-xl navbar-dark bg-dark">
-      <a class="navbar-brand" href="#">Expand at xl</a>
+      <a class="navbar-brand" href="/">Foothills Climbing Community</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample06" aria-controls="navbarsExample06" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
 
       <div class="collapse navbar-collapse" id="navbarsExample06">
         <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
+          <li class="nav-item">
             <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
           </li>
-          {{-- <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li> --}}
-          {{-- <li class="nav-item">
-            <a class="nav-link disabled" href="#">Disabled</a>
-          </li> --}}
+          <li class="nav-item">
+            <a class="nav-link" href="/events">Events</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/about">About Us</a>
+          </li>
+           <li class="nav-item">
+            <a class="nav-link" href="/contact">Contact Us</a>
+          </li>
           <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="https://example.com" id="dropdown06" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+            <a class="nav-link dropdown-toggle" href="" id="dropdown06" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Menu</a>
             <div class="dropdown-menu" aria-labelledby="dropdown06">
               <a class="dropdown-item" href="/about">About Us</a>
               <a class="dropdown-item" href="/events">Events</a>
-              <a class="dropdown-item" href="#">Contact Us</a>
+              <a class="dropdown-item" href="/contact">Contact Us</a>
             </div>
           </li>
+       
         </ul>
         <form class="form-inline my-2 my-md-0">
           <input class="form-control" type="text" placeholder="Search">
--- resources/views/pages/events.blade.php
+++ resources/views/pages/events.blade.php
@@ -4,5 +4,6 @@
         <p>
 
             <!-- add google calendar to track events -->
+            <iframe src="https://calendar.google.com/calendar/embed?src=1eh8ct9kuig9i97sfrf6qud8to%40group.calendar.google.com&ctz=America%2FDenver" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
         </p>
     @endsection
